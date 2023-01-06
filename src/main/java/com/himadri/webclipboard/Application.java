82e5b53779473604c6707c47a58954b3f7e966a0
Csaba Kavai <csaba.kavai.osv@fedex.com>
2023-01-06 12:26:56 +0100

    Native image

--- src/main/java/com/himadri/webclipboard/Application.java
+++ src/main/java/com/himadri/webclipboard/Application.java
@@ -4,20 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-import java.util.Collections;
-
 @SpringBootApplication
 @PropertySource("classpath:/secrets.yml")
 public class Application {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Application.class);
-        if (System.getenv("PORT") != null) {
-            String port = System.getenv("PORT");
-            app.setDefaultProperties(Collections.singletonMap("server.port", port));
-        }
-
-        app.run(args);
+        SpringApplication.run(Application.class, args);
     }
-
-
 }
